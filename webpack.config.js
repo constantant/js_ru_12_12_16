e6e600e368c70de0c8ee42a66dca06f222842f96
constantant <constantant@mail.ru>
2016-12-16 20:40:02 +0300

    solution ts

--- webpack.config.js
+++ webpack.config.js
@@ -6,34 +6,34 @@ const entryPath = path.join(process.cwd(), 'src');
 const outputPath = path.join(process.cwd(), 'build');
 
 module.exports = {
-    devtool: 'source-map',
-    entry: {
-    	app: path.join(entryPath, 'app.tsx')
-    },
-    output: {
-        path: outputPath,
-        filename: 'bundle.js',
-        publicPath: '/static/'
-    },
-	resolve: {
-		extensions: [ ".ts", ".tsx", ".js"]
+	devtool: 'source-map',
+	entry: {
+		app: path.join(entryPath, 'app.tsx')
 	},
-    module: {
-	    rules: [
-		    {
-		    	test: /\.(tsx)?$/,
-			    loader: "awesome-typescript-loader"
-		    }
-	    ],
-	    plugins: [
-		    new CheckerPlugin()
-	    ],
-        loaders: [
-            {
-                test: /\.js/,
-                loaders: ['babel'],
-                include: path.join(__dirname, 'src')
-            }
-        ]
-    }
+	output: {
+		path: outputPath,
+		filename: 'bundle.js',
+		publicPath: '/static/'
+	},
+	resolve: {
+		extensions: [".ts", ".tsx", ".js"]
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(tsx)?$/,
+				loader: "awesome-typescript-loader"
+			}
+		],
+		loaders: [
+			{
+				test: /\.js/,
+				loaders: ['babel'],
+				include: path.join(__dirname, 'src')
+			}
+		]
+	},
+	plugins: [
+		new CheckerPlugin()
+	]
 };
