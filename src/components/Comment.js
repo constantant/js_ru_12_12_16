64dbc0fb19b9f204ecf35e6c3d2e2ad8782c7827
Roman <kaboom.ua@gmail.com>
2016-12-18 18:18:28 +0200

    Update Comment.js

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 export default class Comment extends Component {
 	render() {
 		const {user, text} = this.props.data;
+	    //лучше не вносить li в Comment, это значительно уменьшает возможность переиспользовать компонент
 		return (
 			<li>
 				<h4>{user}</h4>
